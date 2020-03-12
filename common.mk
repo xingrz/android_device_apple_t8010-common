0c507e3b9e23de15823a366a6ded352f9aca7123
XiNGRZ <chenxingyu92@gmail.com>
2020-03-12 10:31:07 +0800

    t8010: Enable virtual nav keys
    Change-Id: I053e9eb781f0167432988e54ca1cefc897d50b41

--- common.mk
+++ common.mk
@@ -6,6 +6,9 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_common.mk)
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
